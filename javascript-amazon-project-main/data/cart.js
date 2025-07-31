58d87539925f4e18a83e9eea1117a019880afb50
Sarthak Chaurasia <sarthak.888888@gmail.com>
2025-08-01 00:12:54 +0530

    move addToCart into cart.js

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -1,3 +1,22 @@
 export const cart=[
     
 ];
+
+
+export function addToCart(productId){
+     let matchingItem;
+            cart.forEach((cartitem)=>{
+                if(productId=== cartitem.productId){
+                    matchingItem= cartitem;
+                }
+            });
+            if(matchingItem){
+              matchingItem.quantity++;
+            }
+             else{
+               cart.push({
+                productId: productId,
+                quantity:1
+            });
+          }
+}
