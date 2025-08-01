0ed7039ecbe424a99b27a1484ed09cac497525f8
Sarthak Chaurasia <sarthak.888888@gmail.com>
2025-08-01 17:44:40 +0530

    fixed the error in delete products from cart

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -18,7 +18,8 @@ cart.forEach((cartItem)=>{
 
     cartSummaryHTML+=
         `
-         <div class="cart-item-container">
+         <div class="cart-item-container
+         js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -41,7 +42,7 @@ cart.forEach((cartItem)=>{
                   <span class="update-quantity-link link-primary">
                     Update
                   </span>
-                  <span class="delete-quantity-link link-primary  js-delete-link" data-product-id='${matchingProduct.id};'>
+                  <span class="delete-quantity-link link-primary  js-delete-link" data-product-id='${matchingProduct.id}'>
                     Delete
                   </span>
                 </div>
@@ -103,9 +104,16 @@ document.querySelector('.js-order-summary').innerHTML=cartSummaryHTML;
 document.querySelectorAll('.js-delete-link').forEach((link)=>{
     link.addEventListener('click',()=>{
       const productId=link.dataset.productId;
-      removeFromCart(productId);
       
-    })
-})
+      removeFromCart(productId);
+      console.log(cart);
+
+      // const container=document.querySelector(`.js-cart-item-container-${productId}`
+
+      // );
+      // console.log(container);
+      
+    });
+});
 
 console.log(cartSummaryHTML);
